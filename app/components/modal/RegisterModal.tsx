34f536689e197d119d02b00c1cbae7213d1c4c70
ngodingbentar <dewaqintoro@gmail.com>
2023-11-15 20:40:44 +0700

    updated: registerModal

--- app/components/modal/RegisterModal.tsx
+++ app/components/modal/RegisterModal.tsx
@@ -12,6 +12,8 @@ import {
 } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -32,11 +34,12 @@ const RegisterModal = () => {
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log('onsubmit data', data);
     setIsLoading(true);
 
     axios.post('/api/register', data)
     .then(() => {
-      // toast.success('Registered!');
+      toast.success('Registered!');
       registerModal.onClose();
       // loginModal.onOpen();
     })
@@ -48,12 +51,47 @@ const RegisterModal = () => {
     })
   }
 
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome to NbVacation"
+        subtitle="Create an account!"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
       isOpen={registerModal.isOpen}
       title="Register"
       actionLabel="Continue"
+      body={bodyContent}
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
     />
