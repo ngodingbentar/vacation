b172682daa5ed79284c4cb17d84d82ad5297da25
ngodingbentar <dewaqintoro@gmail.com>
2023-11-20 01:49:12 +0700

    added: setup upload image with cloudinary

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -10,6 +10,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -167,6 +168,21 @@ const RentModel = () => {
           value={bathroomCount}
           title="Bathrooms" 
           subtitle="How many bathrooms do you have?"
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
         />
       </div>
     )
