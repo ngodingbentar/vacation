631feb9847adfbfbafd8734edbe0a8519e1c2dbc
ngodingbentar <dewaqintoro@gmail.com>
2023-12-05 13:28:08 +0700

    added: post listing

---
+++ app/api/listings/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/action/getCurrentUser";
+
+export async function POST(
+  request: Request, 
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  console.log('body 1', body)
+
+  const { 
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+   } = body;
+   console.log('body 2', body)
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id
+    }
+  });
+
+  console.log('listing', listing)
+
+  return NextResponse.json(listing);
+}
--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -7,11 +7,13 @@ import Modal from "./Modal"
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
 import Input from "../inputs/Input";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 enum STEPS {
   CATEGORY = 0,
@@ -78,6 +80,29 @@ const RentModel = () => {
 
   const onNext = () => {
     setStep((value) => value + 1);
+  }
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+    
+    setIsLoading(true);
+
+    axios.post('/api/listings', data)
+    .then(() => {
+      toast.success('Listing created!');
+      router.refresh();
+      reset();
+      setStep(STEPS.CATEGORY)
+      rentModal.onClose();
+    })
+    .catch(() => {
+      toast.error('Something went wrong.');
+    })
+    .finally(() => {
+      setIsLoading(false);
+    })
   }
 
   const actionLabel = useMemo(() => {
@@ -217,11 +242,32 @@ const RentModel = () => {
     )
   }
 
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night?"
+        />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice 
+          type="number" 
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       title="Airbnb your home!"
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
