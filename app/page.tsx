ba779638cd880cc4e2378e4635b48232e2c75d22
ngodingbentar <dewaqintoro@gmail.com>
2023-12-05 13:43:47 +0700

    Added: empty state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
 import Image from 'next/image'
+import ClientOnly from './components/ClientOnly'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState';
 
 export default function Home() {
+  const listings = []
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
   return (
-    <div className='bg-red-100'>dew</div>
+    <ClientOnly>
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          hah
+          {/* {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))} */}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
